0c78b63a8f1793a13b4ee6b462b344491b73d4a3
EmptyPit <lozovyyandrew@gmail.com>
2019-09-13 15:03:45 +0300

    Update

--- color.py
+++ color.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-end_color = '\033[m'
+end_color = '\033[0m'
 
 color_list = {'black': '\033[30m', 'red': '\033[31m', 'green': '\033[32m', 'yellow': '\033[33m', 'blue': '\033[34m',
             'purple': '\033[35m', 'cyan': '\033[36m', 'white': '\033[37m'}
 
 
 def colr(color, text):
-    com = [v + text + end_color for k, v in color_list.items() if k == color]
-    return com
-    # return ''.join(com)
+    if color in color_list:
+        return color_list[color] + text + end_color
+    elif color not in color_list:
+        return text
 
 
-test = colr('cyan','some text')
-print(*test, end="\n")
+test = colr('purple', 'some text')
+print(test)
